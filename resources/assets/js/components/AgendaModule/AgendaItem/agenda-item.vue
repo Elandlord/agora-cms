1d5d6a9371a49c28d165f0d4c84a934abdedfb05
Melvin Tehubijuluw <melvin.tehubijuluw@beeproger.com>
2017-02-08 19:45:50 +0100

    Agenda-item component
    added agenda-item.vue

---
+++ resources/assets/js/components/AgendaModule/AgendaItem/agenda-item.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+  <div class="date">
+    <P>ZA</P>
+    <p>10 dec</p>
+    <p>19.00 UUR</p>
+  </div>
+  <div class="image"></div>
+  <div class="content">
+    <h5>Titel van het item</h5>
+    <p>Stukje beschrijving van het evenement</p>
+    <button>lees meer</button>
+  </div>
+</div>
+</template>
+
+<style type="text/css" scoped>
+
+</style>
+
+<script >
+  export default {
+
+  }
+</script>
